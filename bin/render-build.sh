2d6cccf2cbcfa3ac48296c7686587603f7b7b38d
yuki <kinkochan39@gmail.com>
2025-03-25 06:24:47 +0900

    assets設定を戻した

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:clobber
-bundle exec rails assets:clean
 bundle exec rails assets:precompile
+bundle exec rails assets:clean
 bundle exec rails db:migrate
