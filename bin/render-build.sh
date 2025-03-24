11a79378daf991d321e2fde0485824d413e403ee
yuki <kinkochan39@gmail.com>
2025-03-25 06:14:40 +0900

    assets関係のコマンド追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
+bundle exec rails assets:clobber
 bundle exec rails assets:clean
+bundle exec rails assets:precompile
 bundle exec rails db:migrate
