8f7365eadf6d658b699d981f2f57e39c3f855c18
yuki <kinkochan39@gmail.com>
2025-03-27 00:19:15 +0900

    管理者の作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.production? && !User.exists?(email: ENV['ADMIN_EMAIL'])
+  User.create!(
+    email: ENV['ADMIN_EMAIL'],
+    password: ENV['ADMIN_PASSWORD'],
+    admin: true
+  )
+end
